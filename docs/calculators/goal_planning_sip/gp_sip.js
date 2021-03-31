afd4ab6777e0383386bdca161d74e861466025be
puneetbaviskar@gmail.com <puneetbaviskar@gmail.com>
2021-03-31 22:34:50 +0530

    SIP cal created

--- docs/calculators/goal_planning_sip/gp_sip.js
+++ docs/calculators/goal_planning_sip/gp_sip.js
@@ -1,53 +1,82 @@
-/* globals Chart:false, feather:false */
+window.onload = () => {
+	let button = document.querySelector("#btn");
 
-(function () {
-  'use strict'
+	// Function for calculating BMI
+	button.addEventListener("click", calculateBMI);
+};
 
-  feather.replace()
+function calculateBMI() {
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
+	/* Getting input from user into height variable.
+	Input is string so typecasting is necessary. */
+	let amount = parseInt(document
+			.querySelector("#height").value);
+
+	/* Getting input from user into weight variable.
+	Input is string so typecasting is necessary.*/
+	let period = parseInt(document
+			.querySelector("#weight").value);
+			
+			let ret = parseInt(document
+			.querySelector("#ret").value);
+
+	let result = document.querySelector("#result");
+
+	// Checking the user providing a proper
+	// value or 
+
+
+
+		// Fixing upto 2 decimal places
+		let monthlyRate = (ret / 12/100);
+		   let months =period * 12;
+			let futureValue=amount * (Math.pow(1 + monthlyRate, months) - 1) ;
+						let invested=months * amount;
+
+			let wealth_gain=futureValue-invested;
+
+		// Dividing as per the bmi conditions
+		result.innerHTML =
+			`<b>Expected Amount</b> : <span>${futureValue}</span>
+			<br><b>Amount Invested:</b> <span>${invested}</span>
+			<br><b>Profit Amount : </b><span>${wealth_gain}</span>`
+			
+
+	
+	}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
     data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
+        labels: ['Expected Amount', 'Amount Invested', 'Profit Amount'],
+        datasets: [{
+            label: '# of Votes',
+            data: [8,2,2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+         
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     }
-  })
-})()
+});
